525ff2a4c0273863cc41f0938a4fb5df93c72d55
c-doo95 <116585057+c-doo95@users.noreply.github.com>
2023-12-07 14:49:49 -0500

    merge digits, begin total counter

--- day1/problem1.js
+++ day1/problem1.js
@@ -9,3 +9,10 @@ const lastDigit = reverseString(str).match(/[0-9]/);
 
 console.log(firstDigit);
 console.log(lastDigit);
+
+let value = Number("" + firstDigit + lastDigit);
+console.log(value);
+
+let total = value + 15;
+
+console.lo
